eb8ce100ce87717eb34d34af88dd89c41e39d734
Knean <mario009c@yahoo.com>
2019-12-09 21:36:57 +0000

    dunno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { markParentViewsForCheck } from '@angular/core/src/view/util';
+declare const d3;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'milosWebsite';
+
+  ngOnInit() {
+    var data = [
+      { name: "mario", parent: "knean" },
+      { name: "knean" },
+      { name: "gay", parent: "knean" },
+      {name: "fag",parent:"knean"}
+    ]
+    const dims = { height: 500, width: 1100 };
+
+    const svg = d3.select('.canvas')
+      .append('svg')
+      .attr('width', dims.width + 100)
+      .attr('height', dims.height + 100);
+
+    const graph = svg.append('g')
+      .attr('transform', 'translate(50, 50)');
+    var rootNode = d3.stratify()
+
+      .id(function (d) {
+        return d.name
+      })
+      .parentId(function (d) {
+        return d.parent;
+      })
+      (data)
+    var treeData = d3.tree().size([800,400])(rootNode)
+    console.log()
+    const nodes = graph.selectAll('.node')
+      .data(treeData.descendants())
+
+    var enterNodes = nodes.enter()
+    .append('rect')
+    .attr('fill', 'red')
+    .attr('stroke','black')
+    .attr('width', 100)
+    .attr('height',100)
+    .attr('transform',(d,i,n)=>{
+      let x = d.x
+      let y = d.y
+      return `translate(${x},${y})`
+    })
+
+
+  }
 }
+
+var draw = function () {
+
+}
+draw();
